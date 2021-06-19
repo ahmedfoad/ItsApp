e398e0a86fef4075c6106e09d417d3cd4b908129
Ahmed Foad <ahmedfoad@echotechit.com>
2021-06-19 02:12:46 +0200

    Revert "fix packages"
    This reverts commit a0e3e03cf54b1bb3f7d9cf12e3f391ada084c11a.

--- angular/src/app/informations/edit-information/edit-information.component.ts
+++ angular/src/app/informations/edit-information/edit-information.component.ts
@@ -4,26 +4,23 @@ import {
   OnInit,
   EventEmitter,
   Output,
-} from "@angular/core";
-import { finalize } from "rxjs/operators";
-import { BsModalRef } from "ngx-bootstrap/modal";
-import { AppComponentBase } from "@shared/app-component-base";
+} from '@angular/core';
+import { finalize } from 'rxjs/operators';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+import { AppComponentBase } from '@shared/app-component-base';
 import {
   InformationServiceServiceProxy,
-  InformationDto,
-} from "@shared/service-proxies/service-proxies";
-import { forEach as _forEach, map as _map } from "lodash-es";
+  InformationDto
+} from '@shared/service-proxies/service-proxies';
+import { forEach as _forEach, map as _map } from 'lodash-es';
 
 @Component({
-  selector: "app-edit-information",
-  templateUrl: "./edit-information.component.html",
-  styleUrls: ["./edit-information.component.css"],
+  selector: 'app-edit-information',
+  templateUrl: './edit-information.component.html',
+  styleUrls: ['./edit-information.component.css'],
   providers: [InformationServiceServiceProxy],
 })
-export class EditInformationComponent
-  extends AppComponentBase
-  implements OnInit
-{
+export class EditInformationComponent   extends AppComponentBase implements OnInit {
   information = new InformationDto();
   saving = false;
   currentStep: number;
@@ -40,26 +37,30 @@ export class EditInformationComponent
 
   ngOnInit(): void {
     debugger;
-    this.informationService.get(this.id).subscribe((result: InformationDto) => {
-      this.information = result;
-    });
+    this.informationService
+      .get(this.id)
+      .subscribe((result: InformationDto) => {
+        this.information = result;
+     });
   }
+
 
   save(): void {
     debugger;
     this.saving = true;
     this.information.stepIndex = this.currentStep;
     this.informationService
-      .createOrUpdate(this.information)
+    .createOrUpdate(this.information)
       .pipe(
         finalize(() => {
           this.saving = false;
         })
       )
       .subscribe(() => {
-        this.notify.info(this.l("SavedSuccessfully"));
+        this.notify.info(this.l('SavedSuccessfully'));
         this.bsModalRef.hide();
         this.onSave.emit();
       });
   }
+
 }
